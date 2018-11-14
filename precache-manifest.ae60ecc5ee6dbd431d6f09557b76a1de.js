f0c55cd9f0647cd50a726dbc1a6c4c2b0b6b3bc8
rmaus8 <rmausolf06@gmail.com>
2018-11-14 16:05:41 -0600

    Updates

---
+++ precache-manifest.ae60ecc5ee6dbd431d6f09557b76a1de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5ef440c3ea91c6e65696ad8f8a090af",
+    "url": "/RMaus8/NetflixClone/static/media/Capture.a5ef440c.JPG"
+  },
+  {
+    "revision": "98c10933343968d62e10",
+    "url": "/RMaus8/NetflixClone/static/js/runtime~main.98c10933.js"
+  },
+  {
+    "revision": "387e05136a066fe3e18c",
+    "url": "/RMaus8/NetflixClone/static/js/main.387e0513.chunk.js"
+  },
+  {
+    "revision": "faec9e6ead2fc94ed01c",
+    "url": "/RMaus8/NetflixClone/static/js/1.faec9e6e.chunk.js"
+  },
+  {
+    "revision": "387e05136a066fe3e18c",
+    "url": "/RMaus8/NetflixClone/static/css/main.e6cf6109.chunk.css"
+  },
+  {
+    "revision": "2886b0a2907e291568658d46715eca65",
+    "url": "/RMaus8/NetflixClone/index.html"
+  }
+];
