5074a3a4baf881c00ab57520f8145c27a6faad61
rmaus8 <rmausolf06@gmail.com>
2018-11-14 16:07:16 -0600

    Netflix clone completed

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
-
-import NavBar from '../../components/Navigation/NavBar/NavBar'
 import styles from './Layout.module.css'
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+
 
 class Layout extends Component {
-    state = {
-        showSideDrawer: false
-    }
-    
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
-    }
-    
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return {showSideDrawer: !prevState.showSideDrawer};
-        }) 
-    }
     
     render () {
         return (
             <div className={styles.Layout}>
-                <NavBar sideDrawerToggle={this.sideDrawerToggleHandler}/>
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
                 <main className={styles.Main}>
                     {this.props.children}
                 </main>
